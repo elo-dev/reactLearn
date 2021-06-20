9a6781beae097ef4843238767719c145928cfbca
Dima Frolov <dimasiys25@gmail.com>
2021-06-20 17:22:44 +0300

    Create pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,14 +6,35 @@ import userPhoto from '../../assets/images/png-transparent-avatar-user-computer-
 class Users extends React.Component{
 
     componentDidMount(){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items)
         })
     }
 
     render(){
+
+        let pageCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+
+        let pages = []
+        for(let i = 1; i <= pageCount; i++){
+            pages.push(i)
+        }
+
         return (
             <section className={style.usersSection}>
+                <div className={style.pageCount}>
+                    {pages.map(p => {
+                        return <span className={`${style.notSelectedPage} ${this.props.currentPage === p && style.selectedPage}`} onClick={(e) => {this.onPageChanged(p)}}>{p}</span>
+                    })}
+                </div>
                 {this.props.users.map(u => <div className={style.usersWrapper} key={u.id}>
                     <div className={style.usersFollow}>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
