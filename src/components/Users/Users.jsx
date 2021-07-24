2d8c64fdf5df1afd81edad4b9cbc0894d71d5e1c
Dima Frolov <dimasiys25@gmail.com>
2021-07-24 21:53:47 +0300

    Preloader

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,29 @@
-import * as axios from 'axios'
 import React from 'react'
 import style from './Users.module.css'
 import userPhoto from '../../assets/images/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png'
 
-class Users extends React.Component{
+let Users = (props) =>{
 
-    componentDidMount(){
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
-        })
+    let pageCount = Math.ceil(props.totalUsersCount / props.pageSize)
+
+    let pages = []
+    for(let i = 1; i <= pageCount; i++){
+        pages.push(i)
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        })
-    }
-
-    render(){
-
-        let pageCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-
-        let pages = []
-        for(let i = 1; i <= pageCount; i++){
-            pages.push(i)
-        }
-
-        return (
-            <section className={style.usersSection}>
+    return(
+        <section className={style.usersSection}>
                 <div className={style.pageCount}>
                     {pages.map(p => {
-                        return <span className={`${style.notSelectedPage} ${this.props.currentPage === p && style.selectedPage}`} onClick={(e) => {this.onPageChanged(p)}}>{p}</span>
+                        return <span className={`${style.notSelectedPage} ${props.currentPage === p && style.selectedPage}`} onClick={(e) => {props.onPageChanged(p)}}>{p}</span>
                     })}
                 </div>
-                {this.props.users.map(u => <div className={style.usersWrapper} key={u.id}>
+                {props.users.map(u => <div className={style.usersWrapper} key={u.id}>
                     <div className={style.usersFollow}>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
                         {u.followed 
-                        ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button> 
-                        : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>
+                        ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> 
+                        : <button onClick={() => {props.follow(u.id)}}>Follow</button>
                         }
                     </div>
                     <div className={style.usersInfo}>
@@ -55,8 +38,7 @@ class Users extends React.Component{
                     </div>
                 </div>)}
             </section>
-        )
-    }
+    )
 }
 
 export default Users
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
 
 let initialState = {
   users: [],
   pageSize: 5,
   totalUsersCount: 0,
-  currentPage: 1
+  currentPage: 1,
+  isFetching: true
 }
 
 const UsersReducer = (state = initialState, action) => {
@@ -40,6 +42,8 @@ const UsersReducer = (state = initialState, action) => {
         return {...state, currentPage: action.currentPage}
       case SET_TOTAL_USERS_COUNT: 
         return {...state, totalUsersCount: action.count}
+      case TOGGLE_IS_FETCHING: 
+        return {...state, isFetching: action.isFetching}
       default: 
         return state
     }
@@ -77,6 +81,13 @@ export const setTotalUsersCountActionCreator = (totalUsers) => {
   return {
     type: SET_TOTAL_USERS_COUNT,
     count: totalUsers
+  }
+}
+
+export const toggleIsFetching = (isFetching) => {
+  return{
+    type: TOGGLE_IS_FETCHING,
+    isFetching
   }
 }
 
